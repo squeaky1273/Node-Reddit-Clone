9acaf15318ea022307211880619bbcb56c907189
squeaky1273 <pjriddell22@gmail.com>
2020-02-03 15:21:59 -0800

    Users can view list of posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,20 @@
 const Post = require('../models/post');
 
 module.exports = app => {
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
+  app.post("/posts/new", (req, res) => {
     const post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB
     post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
+        return res.redirect(`/`);
     })
-  });
+});
 
-};
+app.get("/", (req, res) => {
+    Post.find({})
+    .then(posts => {
+        res.render("posts-index", {posts});
+    })
+    .catch(err => {
+        console.log('error', err.message);
+    });
+})};
