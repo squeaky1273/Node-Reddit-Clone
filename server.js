bbc04c2a9bbfe0d6db16649b3b735b15a4cc14dc
squeaky1273 <pjriddell22@gmail.com>
2020-02-03 15:12:24 -0800

    got mongoose to work

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const express = require('express');
 // App Setup
 const app = express();
 
+//Controllers
+require('./controllers/posts.js')(app);
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
 // Somewhere near the top
 app.use(express.static('public'))
 
 // Middleware
 const exphbs  = require('express-handlebars');
+
+// Set db
+require('./data/reddit-db');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -16,6 +32,10 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   // render the main view
   res.render('home');
+})
+app.get('/posts/new', (req, res) => {
+  // render the post view
+  res.render('posts-new');
 })
 
 app.listen(3000, () => {
