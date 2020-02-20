a1e3f6da54dd1ed3f76b1509c93cb456f91e9396
squeaky1273 <pjriddell22@gmail.com>
2020-02-20 01:34:00 -0800

    refactored populate code

--- controllers/posts.js
+++ controllers/posts.js
@@ -49,37 +49,27 @@ app.post("/posts/new", (req, res) => {
   }
 });
 
-// SUBREDDIT
-app.get("/n/:subreddit", function (req, res) {
-  var currentUser = req.user;
-  Post.find({ subreddit: req.params.subreddit }).populate('author')
-      .then(posts => {
-          res.render("posts-index", { posts, currentUser });
-      })
-      .catch(err => {
-          console.log(err);
-      });
-      // LOOK UP THE POST
-    Post.findById(req.params.id).populate('comments')
-      .then((post) => {
-        res.render('post-show.hbs', { post })
-      })
-      .catch((err) => {
-        console.log(err.message)
-      })
-  });
-  
 // SHOW
 app.get("/posts/:id", function (req, res) {
   var currentUser = req.user;
-  // LOOK UP THE POST
-
-  Post.findById(req.params.id).populate({path:'comments', populate: {path: 'author'}}).populate('author')
+  Post.findById(req.params.id).populate('comments').lean()
       .then(post => {
           res.render("posts-show", { post, currentUser });  
       })
       .catch(err => {
           console.log(err.message);
+      });
+});
+
+// SUBREDDIT
+app.get("/n/:subreddit", function (req, res) {
+  var currentUser = req.user;
+  Post.find({ subreddit: req.params.subreddit }).lean()
+      .then(posts => {
+          res.render("posts-index", { posts, currentUser });
+      })
+      .catch(err => {
+          console.log(err);
       });
 });
 }
--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Populate = require("../utils/autopopulate");
 
 const CommentSchema = new Schema({
   author : { type: Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true }
 });
+
+// Always populate the author field
+CommentSchema
+    .pre('findOne', Populate('author'))
+    .pre('find', Populate('author'))
 
 module.exports = mongoose.model("Comment", CommentSchema);
